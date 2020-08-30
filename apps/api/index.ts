6f3a074c0256d43c9a1da36e98f23ae22185e6bf
Michele Memoli <michele@100shapes.com>
2020-08-30 19:36:08 +0100

    Initial commit

---
+++ apps/api/index.ts
@@ -0,0 +1,38 @@
+import { NestFactory } from '@nestjs/core';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import { INestApplication } from '@nestjs/common';
+import { AppModule } from './src/app.module';
+import express from 'express';
+import { Express } from 'express';
+import { Server } from 'http';
+import { APIGatewayEvent, Context } from 'aws-lambda';
+import { createServer, proxy, Response } from 'aws-serverless-express';
+
+export async function createApp(
+  expressApp: Express,
+): Promise<INestApplication> {
+  return await NestFactory.create(AppModule, new ExpressAdapter(expressApp));
+}
+
+let cachedServer: Server;
+
+async function bootstrap(): Promise<Server> {
+  const expressApp = express();
+
+  const app = await createApp(expressApp);
+  await app.init();
+  app.enableCors();
+
+  return createServer(expressApp);
+}
+
+export async function handler(
+  event: APIGatewayEvent,
+  context: Context,
+): Promise<Response> {
+  if (!cachedServer) {
+    cachedServer = await bootstrap();
+  }
+
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+}
