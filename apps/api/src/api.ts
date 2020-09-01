c198c35ef82cb06579ab9b26b25f2efe46c69268
Michele Memoli <michele@100shapes.com>
2020-09-01 17:03:58 +0100

    Stuff and things

---
+++ apps/api/src/api.ts
@@ -0,0 +1,31 @@
+import { Handler, Context } from 'aws-lambda';
+import { Server } from 'http';
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { ExpressAdapter } from '@nestjs/platform-express';
+import serverless, { proxy } from 'aws-serverless-express';
+
+let cachedServer: Server;
+
+process.on('unhandledRejection', reason => {
+  // tslint:disable-next-line:no-console
+  console.error(reason);
+});
+
+process.on('uncaughtException', reason => {
+  // tslint:disable-next-line:no-console
+  console.error(reason);
+});
+
+function bootstrapServer(): Promise<Server> {
+  const expressApp = require('express')();
+  const adapter = new ExpressAdapter(expressApp);
+  return NestFactory.create(AppModule, adapter, { logger: false })
+    .then(app => app.init())
+    .then(() => serverless.createServer(expressApp));
+}
+
+export const handler: Handler = async (event: any, context: Context) => {
+  cachedServer = await bootstrapServer();
+  return proxy(cachedServer, event, context, 'PROMISE').promise;
+};
